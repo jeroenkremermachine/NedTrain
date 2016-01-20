373573fa24f03e102bb62e53eba66ed6f686556a
Viviane van Tol <vivianevantol@gmail.com>
2016-01-20 14:06:27 +0100

    Finished composition class

--- src/trainComposition.java
+++ src/trainComposition.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
 
 public class trainComposition {
+	private ArrayList<Train> trains = new ArrayList<Train>();
+	private int ID;
 
+	public trainComposition(ArrayList trains, int ID){
+		this.trains = trains;
+		this.ID = ID;
+	}
+	
+	public int getID(){
+		return ID;
+	}
+	
+	public int getNumber(){
+		return trains.size();
+	}
+	
+	public double getLength(){
+		double length = 0;
+		for (int i=0; i<trains.size() ;i++){
+			length = length + trains.get(i).getType().getLength();
+		}
+		return length;
+	}
+	
+	public void addTrain(Train x){
+		trains.add(x);
+	}
+	
+	public void removeTrain(Train x){
+		trains.remove(x);
+	}
 }
