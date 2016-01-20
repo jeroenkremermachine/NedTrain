d91adedd15955b2f7f039cafdcb425bc4cc4bf84
Viviane van Tol <vivianevantol@gmail.com>
2016-01-20 13:35:04 +0100

    Test voor Silvana

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
 		
 		System.out.println(myTrain.getWash());
 		System.out.println(myTrain.getType().getLength());
+		
+		System.out.println("Dit moet silvana kunnen zien!");
 	}
 
 }
