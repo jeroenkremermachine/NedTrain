7969a00ba49d34383a798a9efa798abb1c9d4752
SilvanaG2 <silvanadeleeuw@hotmail.com>
2016-01-29 12:14:31 +0100

    Probabilities en durations toegevoegd

--- src/trainType.java
+++ src/trainType.java
@@ -1,17 +1,27 @@
 
 public class trainType {
-	private double length;
-	private double inspectionTime;
-	private double repairTime;
-	private double cleaningTime;
-	private double washingTime;
+	public double length;
+	public double inspectionTime;
+	public double repairTime;
+	public double cleaningTime;
+	public double washingTime;
+	public double inspectionProb;
+	public double repairProb;
+	public double cleanProb;
+	public double washProb;
+	public double interchangeProb;
 
-	public trainType(double length, double inspectiontime, double repairtime, double cleaningtime, double washingtime){
+	public trainType(double length, double inspectiontime, double repairtime, double cleaningtime, double washingtime, double inspectionProb, double repairProb, double cleanProb,double washProb,double interchangeProb){
 		this.length = length;
 		this.inspectionTime = inspectiontime;
 		this.repairTime = repairtime; 
 		this.cleaningTime = cleaningtime;
 		this.washingTime = washingtime;
+		this.inspectionProb=inspectionProb;
+		this.repairProb=repairProb;
+		this.cleanProb=cleanProb;
+		this.washProb=washProb;
+		this.interchangeProb=interchangeProb;
 	}
 
 	public double getLength(){
@@ -32,6 +42,26 @@ public class trainType {
 
 	public double getWashingtime(){
 		return washingTime;
+	}
+	
+	public double getInspectionprob(){
+		return inspectionProb;
+	}
+	
+	public double getRepairprob(){
+		return repairProb;
+	}
+	
+	public double getCleanprob(){
+		return cleanProb;
+	}
+	
+	public double getWashprob(){
+		return washProb;
+	}
+	
+	public double getInterchangeprob(){
+		return interchangeProb;
 	}
 	
 }
