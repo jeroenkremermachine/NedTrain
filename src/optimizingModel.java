7e619408e23d00fdaacc56674cfb3c5887373f6b
jeroenkremermachine <jeroen_kremer@hotmail.com>
2016-02-02 16:14:07 +0100

    heuristiek outline gemaakt

--- src/optimizingModel.java
+++ src/optimizingModel.java
@@ -46,13 +46,15 @@ public class optimizingModel {
 			int[][] d = List.getDeparturelist();
 			int[] departureMin = getMin(d, 0);
 			int end = List.endmovement;//create set methods
+//			int[][] eventlist = List.getEventlist();
+//			int[] eventmin = getMin(eventlist, 0);
 
 			if(arrivalMin[1]==minuut){
 				//do arrival
 				positions.set(0, List.getArrivallist()[arrivalMin[0]][1]); //put train on position
 				List.setArrivallist(Integer.MAX_VALUE, arrivalMin[0]); //set arrival time on inf
 			}
-			if(departureMin[1]==minuut){
+			if(departureMin[1]==minuut){ // check if positie is gevuld EN meerdere posities mogelijk, voor meerdere treinen op departure track
 				//do departure
 				int departurePosition = getIndex(positions, List.getDeparturelist()[departureMin[0]][1]); //find leaving train
 				positions.set(departurePosition, 0); //remove leaving train
@@ -63,11 +65,10 @@ public class optimizingModel {
 				List.setEndmovement(Integer.MAX_VALUE);
 			}
 
-			//asap weg van arrival spoor als arrival track bezet
+			// if (eventmin = minuut){ // nog checken: meerdere events op dezelfde minuut klaar?
+				// zorg dat in eventmatrix de tweede kolom op false komt te staan, de trein heeft geen event meer.
+			// }
 
-			//if arrival set arriving train on position 1 -> create trainevents({inpection, clean, repair}->behandel, wash, departarea ,depart)
-
-			//if departure remove departing train from position x
 
 			if (movement == false){
 				//Check arrival track, niet elke trein komt op hetzelfde spoor aan
@@ -86,23 +87,57 @@ public class optimizingModel {
 							break;
 						}
 						
-					// build movement list
-					// a train is on the track
-					//move train to arrival tracks
-
+					// build movement list (see movement method)
+					// check for inspection (see eventmatrix method)
 				}
 			}
+				
+// bij beide onderstaande methodes, moeten we gaan bedenken waar naar toe de beweging gaat. Mogelijk conflict met de movementlist als je een keuze maakt;			
+//				if (movement == false){
+//					if(positions.get(ONE OF THE EXTERNAL WASHING MACHINES)!=0){ 
+//						int endPosition = -1;
+//						for (int i=0;i<priorityArrival.length;i++){
+//							movementTime = move.possibleMovement(1, priorityArrival[i], positions, Data, Yard);
+//							if(movementTime!=0){
+//								endPosition = priorityArrival[i];
+//								int id = positions.get(0);
+//								positions.set(endPosition, id);
+//								positions.set(1, 0);
+//								timeMovement = minuut + movementTime;
+//								movement = true;
+//								List.setEndmovement(timeMovement);
+//								break;
+//							}
+//					}
+//				}
+//					
+//					
+//					if (movement == false){
+//						if(positions.get(ONE OF THE INTERNAL WASHING MACHINES)!=0){ 
+//							int endPosition = -1;
+//							for (int i=0;i<priorityArrival.length;i++){
+//								movementTime = move.possibleMovement(1, priorityArrival[i], positions, Data, Yard);
+//								if(movementTime!=0){
+//									endPosition = priorityArrival[i];
+//									int id = positions.get(0);
+//									positions.set(endPosition, id);
+//									positions.set(1, 0);
+//									timeMovement = minuut + movementTime;
+//									movement = true;
+//									List.setEndmovement(timeMovement);
+//									break;
+//								}
+//						}
+//					}
 
 			if (movement == false){
-				// check of er bewegingsverzoek op de eventlist staat, bijvoorbeeld nu: 
-				int[][] m = List.getMovementlist();
-				int[] movementMin = getMin(m,0); //what type of movement on which train
+				// nog inbouwen dat als de gewenst vroegste move op de movementlist niet mogelijk is omdat een event uitgevoerd wordt, of hij geblokkeerd wordt, dat de volgende gepakt wordt.
+				int[][] m = List.getMovementlist(); 
+				int[] movementMin = getMin(m,0); // more than 1 movement as minimum? no problem? 
 				int movementType = m[movementMin[0]][1];
 				int movementTrainID = m[movementMin[0]][2];
-				//find current position of train
 				int currentPosition = getIndex(positions, movementTrainID);
 				int endPosition = -1;
-
 			
 				if(movementType ==1){
 					for (int i=0;i<priorityType1.length;i++){
@@ -115,6 +150,7 @@ public class optimizingModel {
 							timeMovement = minuut + movementTime;
 							movement = true;
 							List.setEndmovement(timeMovement);
+							// eventMatrix method uitvoeren
 							break;
 						}
 					}
@@ -130,6 +166,7 @@ public class optimizingModel {
 								timeMovement = minuut + movementTime;
 								movement = true;
 								List.setEndmovement(timeMovement);
+								// eventMatrix method uitvoeren
 								break;
 							}
 						}
@@ -147,21 +184,17 @@ public class optimizingModel {
 							timeMovement = minuut + movementTime;
 							movement = true;
 							List.setEndmovement(timeMovement);
+							// eventMatrix method uitvoeren
 							break;
 						}
 					}
 				} 
-					//do departure
+
 				
 				else if(movementType ==4)
 				{	
 					// moet naar zijn eindspoor toe
 				}
-				
-				//later set eventlist endmovement method
-				//priority of emptying washing machine en platforms (zelfde als beweging naar beginspoor)
-				// if not possible do something else
-
 			}	
 			minuut++;
 			}
@@ -169,6 +202,16 @@ public class optimizingModel {
 
 	}
 
+	
+	// Het moet mogelijk zijn om een event te skippen in het geval er geen tijd meer voor is. 
+	// SKIP EVENT IF: TYPE 4 event - (eventime + minuut) < 0
+	// bouw dit in de eventmatrix method.
+	
+	
+	// methodes
+	
+	
+	
 	public int[] getMin(int[] x){
 		int minvalue = Integer.MAX_VALUE;
 		int index = -1;
@@ -228,6 +271,79 @@ public class optimizingModel {
 		return length;
 	}
 
+// public int getLastMoveTime(int id){ // vult de movementmatrix met uiterlijke tijden voor het rijden naar area's. 
+//
+//		 // per train ID moeten we de volgende dingen uit gaan lezen: 
+//	
+//	 	 // departureTime;
+//		 boolean washExtern = true;
+//		 boolean washIntern = true;
+//		 boolean inspection = true;
+//		 boolean repair = true;
+		 
+		 // type 4: To depart track (Every train)
+		 // type 3: to depart area (every train. some trains get repaired there)
+		 // type 2; to external cleaning area (some trains)
+		 // type 1: to internal cleaning area (some trains)
+		 
+		 
+// SET TYPE 4 EVENT (MOVING TO THE DEPART TRACK)
+		 // get departure time:		
+		 // type4Event = departure time - movementtime to depart track
+		 // fill movement list met type 5 event. 	
+	 
+// --------------------------------------------------------------------------------------------------		 
+// IF BOOLEAN REPAIR = TRUE: SET TYPE 3 TO BE THE ULTIMATE_START_REPAIR_EVENT	
+		 // get type 4 event time: 
+		 // type 3 event  = type4 event - repairtime - movementtime to repair/depart area		
+		 
+// IF BOOLEAN REPAIR = FALSE: SET TYPE 3 TO BE THE TIME THAT YOU HAVE TO GO TO DEPART AREA
+		 // get type 4 event time: 
+		 // type 3 event  = type4 event - movementtime to repair/depart area	 
+		 
+// --------------------------------------------------------------------------------------------------				 
+// IF BOOLEAN CLEANINGEXTERN = TRUE: SET TYPE 2 TO BE THE TIME THAT YOU HAVE TO GO TO THE CLEANING EXTERN AREA		 
+		 // get type 3 event time
+		 // type 2 event: Type 3 event - washing extern time - movement to extern wash area 
 
+// --------------------------------------------------------------------------------------------------		
+// IF BOOLEAN CLEANINGINTERN = TRUE: SET TYPE 1 TO BE THE TIME THAT YOU HAVE TO GO TTHE CLEANING INTERN AREA	
+		 // IF BOOLEAN CLEANING EXTERN = TRUE: 
+		 // type1 move =  type 2 - interncleaning time - movement time to internal cleaning platform
+		 
+		 // IF BOOLEAN CLEANING INTERN = FALSE:
+		 // type1 move =  Type 3 - interncleaning event - movement time to internal cleaning platofrm
+
+// --------------------------------------------------------------------------------------------------
+
+		 
+		 // all types events go to the movement list, they consist of the ultimate time the train has to move
+		 // all other events go to an event list, they correspond to an event is taking place. If an event is taking place it cannot move, AT START TIME MAKE END TIME. 
+		 // Only start an event if type5 - eventtime > 0 . (so there is always time to travel to the departure track). 
+		 // THE REAL start event is making sure that the train cannot move, and that the train makes an end event after which it can move. 
+
+	 
+// return lastMoveTime;
+// 
+// }
+// 
+// public int StartEvent(int id){ // vult de eventlist met events (ANDERS DAN DE MOVEMENTLIST!)  
+//
+//	 // IF A TRAIN ARRIVES, OR IF A END MOVEMENT EVENT OCCURS, WE WILL CALL THIS METHOD. PER TRAIN ID SHOULD EXIST A EVENT MATRIX:
+//	 // matrix:  ID -- CURRENT event -- WASHEXTERN -- WASHINTERN --- INSPECTION -- REPAIR -- event counter
+//	 
+//	 // get current train position
+//	 // depending on position check which boolean should be true
+//	 // if it's false, no action should be taken
+//	 // if it's true, make it false and make current event true
+//	 // eventcounter +1. 
+//	 // set end event time
+//	 
+//	 // VOORDEEL: train cannot move if it's busy with an event
+//	 // VOORDEEL: aan het eind check je het aantal events op true, en de counter om het aantal geslaagde events te bepalen. 
+//
+//return lastMoveTime;
+//
+//}
 }
 
